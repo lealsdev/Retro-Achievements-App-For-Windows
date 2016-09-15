a9eba108a28e871e7937cd166a9a59ed9940127a
LAS82 <lealves_82@yahoo.com.br>
2016-09-15 00:03:51 -0300

    License info

--- RetroAchievementsApp/Utils.cs
+++ RetroAchievementsApp/Utils.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region copyright
+
+// --------------------------------------------------------------------------------------------------------------------
+// Copyright (C) 2016 L.A.S - LAS Soft
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the +terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/. 
+//
+// Utils.cs - General app methods.
+// 
+// Email: lealves_82@yahoo.com.br
+// RetroAchievements username: FitaOuCartucho
+// --------------------------------------------------------------------------------------------------------------------
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
